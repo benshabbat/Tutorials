047209bceff3280173827abae8484fd210592002
benshabbat <benshabbat27@gmail.com>
2023-12-13 22:16:01 +0200

    INTEGER

--- python/variables/variables.py
+++ python/variables/variables.py
@@ -1,4 +1,12 @@
 
 # variable = a reusable container for storing a value
 # a variable behaves as if it were the value it contains
- 
+
+# INTEGER
+age = 21
+players = 2
+quantity = 5
+
+print(f"You are {age} years old")
+print(f"There are {players} players online")
+print(f"You would like to buy {quantity} items")
