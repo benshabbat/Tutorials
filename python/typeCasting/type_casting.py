315190f96724cc55d2b40e860a214bf22e96d90d
benshabbat <benshabbat27@gmail.com>
2023-12-14 23:46:24 +0200

    type casting title

---
+++ python/typeCasting/type_casting.py
@@ -0,0 +1,3 @@
+# type casting = The process of converting a value of one data type to another
+#                          (string, integer, float, boolean)
+#                          Explicit vs Implicit
