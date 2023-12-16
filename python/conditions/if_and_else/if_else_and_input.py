29127c2bed284778eaddd985b31eb9cf59bb32d3
benshabbat <benshabbat27@gmail.com>
2023-12-16 22:38:56 +0200

    more example

--- python/conditions/if_and_else/if_else_and_input.py
+++ python/conditions/if_and_else/if_else_and_input.py
@@ -1,3 +1,4 @@
+# example 1
 response = input("Do you want food (Y/N)?: ")
 
 if response == "Y":
@@ -5,3 +6,10 @@ if response == "Y":
 else:
    print("No food for you!")
 
+# example 2
+name = input("Enter your name: ")
+
+if name == "":
+   print("You did not enter your name!")
+else:
+   print(f"Hello {name}")
