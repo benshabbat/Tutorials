e597b37c6afee8e157562f90075d4134129f2e6b
benshabbat <benshabbat27@gmail.com>
2023-12-19 23:45:44 +0200

    reverse credit_number

---
+++ python/string/examples/example2.py
@@ -0,0 +1,10 @@
+# EXERCISE 1
+credit_number = "1234-5678-9012-3456"
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# EXERCISE 2
+# reverse
+credit_number = "1234-5678-9012-3456"
+credit_number = credit_number[::-1]
+print(credit_number)
