758c9bd50642fbb8d3353d8716d8191a1ea2e689
benshabbat <benshabbat27@gmail.com>
2023-12-20 19:17:10 +0200

    while ex 1

---
+++ python/conditions/while_loop/example1.py
@@ -0,0 +1,13 @@
+
+#while loop = perform some code WHILE some condition remains true
+
+# ---------------- EXAMPLE 1 ----------------
+
+name = input("Enter your name: ")
+
+while name == "":
+   print("You did not enter your name!")
+   name = input("Enter your name: ")
+
+print(f"Hello {name}")
+
