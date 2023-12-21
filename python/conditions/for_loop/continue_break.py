35e72e30e2e0c13a9885b66c96f357716d1a434e
benshabbat <benshabbat27@gmail.com>
2023-12-22 00:45:18 +0200

    continue break

---
+++ python/conditions/for_loop/continue_break.py
@@ -0,0 +1,15 @@
+# ---------------- CONTINUE ----------------
+
+for x in range(1, 21):
+   if x == 13:
+       continue
+   else:
+       print(x)
+
+# ---------------- BREAK ----------------
+
+for x in range(1, 21):
+   if x == 13:
+       break
+   else:
+       print(x)
