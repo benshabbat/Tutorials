6e3eb1f497f8305637bbdf7b448d91118ce2e5bc
benshabbat <benshabbat27@gmail.com>
2023-12-31 21:25:25 +0200

    tests

--- javascript/documentGetElement/main.js
+++ javascript/documentGetElement/main.js
@@ -1,11 +1,11 @@
 console.log(`Hello`);
-console.log(`I like pizza!`);
+console.log(`I am Jewish!`);
 
 window.alert(`This is an alert!`);
-window.alert(`I like pizza!`);
+window.alert(`I am Jewish!`);
 
 document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza!`;
+document.getElementById("myP").textContent = `I am Jewish!`;
 
 // This is a comment
 
