b9148862879f9de4da9de7c1c845620ef28f909b
benshabbat <benshabbat27@gmail.com>
2023-12-31 21:24:26 +0200

    type of comments

--- javascript/documentGetElement/main.js
+++ javascript/documentGetElement/main.js
@@ -7,3 +7,11 @@ window.alert(`I like pizza!`);
 document.getElementById("myH1").textContent = `Hello`;
 document.getElementById("myP").textContent = `I like pizza!`;
 
+// This is a comment
+
+/*
+    This
+    is
+    a
+    comment
+*/
