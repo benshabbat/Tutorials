7c97a2a5812bea7f82ef882bd94959299e89b394
benshabbat <benshabbat27@gmail.com>
2024-01-02 00:02:18 +0200

    easy way

---
+++ javascript/userInput/main.js
@@ -0,0 +1,10 @@
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+// ------------------------- EASY WAY -------------------------
+// let username = window.prompt("What's your username?");
+
+// console.log(username);
+
