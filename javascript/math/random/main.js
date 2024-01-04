c63e1235212e9dcbae5eed092ebc913181eb18d8
benshabbat <benshabbat27@gmail.com>
2024-01-04 17:45:45 +0200

    add comment

--- javascript/math/random/main.js
+++ javascript/math/random/main.js
@@ -2,6 +2,7 @@
 const max = 6;
 const min = 1;
 document.getElementById("rollBtn").onclick = () => {
+   //Random 1-6 numbers 
   document.getElementById("res").textContent =
     Math.floor(Math.random() * max) + min;
 };
