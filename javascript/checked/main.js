f337cc706142ef114e652d490d78050629541396
benshabbat <benshabbat27@gmail.com>
2024-01-04 23:13:32 +0200

    checked done

--- javascript/checked/main.js
+++ javascript/checked/main.js
@@ -1,11 +1,20 @@
-
-// .checked = property that determines the checked state of an 
+// .checked = property that determines the checked state of an
 //                     HTML checkbox or radio button element
 
+const subscribe = document.getElementById("subscribe");
+const israel = document.getElementById("israel");
+const brazil = document.getElementById("brazil");
+const france = document.getElementById("france");
 
-
-
-const subscribe = document.getElementById('subscribe');
-const israel = document.getElementById('israel');
-const brazil = document.getElementById('brazil');
-const france = document.getElementById('france');
+document.getElementById("submit").onclick = () => {
+  subscribe.checked
+    ? (document.getElementById("subres").textContent = "subscribed")
+    : (document.getElementById("subres").textContent = "unsubscribed");
+  israel.checked
+    ? (document.getElementById("cityres").textContent = "israel")
+    : brazil.checked
+    ? (document.getElementById("cityres").textContent = "brazil")
+    : france.checked
+    ? (document.getElementById("cityres").textContent = "france")
+    : "";
+};
