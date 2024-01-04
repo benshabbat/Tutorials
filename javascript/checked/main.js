0c600153a9eda239cb8d5be8a5ac4f32ef0266da
benshabbat <benshabbat27@gmail.com>
2024-01-04 23:06:50 +0200

    html and css done

---
+++ javascript/checked/main.js
@@ -0,0 +1,11 @@
+
+// .checked = property that determines the checked state of an 
+//                     HTML checkbox or radio button element
+
+
+
+
+const subscribe = document.getElementById('subscribe');
+const israel = document.getElementById('israel');
+const brazil = document.getElementById('brazil');
+const france = document.getElementById('france');
