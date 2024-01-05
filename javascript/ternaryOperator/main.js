1a83006147b681d874acecc55d6b328e70dce0cc
benshabbat <benshabbat27@gmail.com>
2024-01-05 09:37:28 +0200

    ex1

---
+++ javascript/ternaryOperator/main.js
@@ -0,0 +1,7 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                                  helps to assign a variable based on a condition
+//                                  condition ? codeIfTrue : codeIfFalse;
+// Example 1
+let time = 9;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+console.log(greeting);
