c948a2fa3228aa0478b4020df3264a634def093a
benshabbat <benshabbat27@gmail.com>
2024-01-05 09:38:26 +0200

    clean code

--- javascript/ternaryOperator/main.js
+++ javascript/ternaryOperator/main.js
@@ -1,6 +1,7 @@
 // ternary operator = a shortcut to if{} and else{} statements
 //                                  helps to assign a variable based on a condition
 //                                  condition ? codeIfTrue : codeIfFalse;
+
 // Example 1
 let time = 9;
 let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
@@ -10,6 +11,7 @@ console.log(greeting);
 let isStudent = false;
 let message = isStudent ? "You are a student" : "You are NOT a student";
 console.log(message);
+
 // Example 3
 let purchaseAmount = 99;
 let discount = purchaseAmount >= 100 ? 10 : 0;
