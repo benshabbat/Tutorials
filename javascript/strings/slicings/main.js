a04d43706a7466e8b7e81d766f6e2fb4f2601081
benshabbat <benshabbat27@gmail.com>
2024-01-06 23:06:24 +0200

    ex2

--- javascript/strings/slicings/main.js
+++ javascript/strings/slicings/main.js
@@ -9,3 +9,11 @@ const firstName = fullName.slice(0, fullName.indexOf(" "));
 const lastName = fullName.slice(fullName.indexOf(" ") + 1);
 console.log(firstName);
 console.log(lastName);
+
+// Example 2
+
+const email = "Benshabbat27@gmail.com";
+const userName = email.slice(0,email.indexOf("@"));
+const extension = email.slice(email.indexOf("@") + 1);
+console.log(userName);
+console.log(extension);
