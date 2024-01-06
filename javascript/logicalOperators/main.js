658c6135dcb70acd3144f3f76074648b7995ba27
benshabbat <benshabbat27@gmail.com>
2024-01-06 23:27:33 +0200

    ex1

---
+++ javascript/logicalOperators/main.js
@@ -0,0 +1,18 @@
+
+// logical operators =  used to combine or manipulate boolean values 
+//                                    (true or false)
+
+// AND = &&
+// OR  = ||
+// NOT = !
+
+// ---------- EXAMPLE 1 ----------
+const temp = -100;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD");
+}
+
