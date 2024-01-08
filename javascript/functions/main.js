215fd8ca7c64699260176d074a72764e742cd65e
benshabbat <benshabbat27@gmail.com>
2024-01-08 21:25:51 +0200

    ex1

---
+++ javascript/functions/main.js
@@ -0,0 +1,15 @@
+// function = A section of reusable code.
+//                    Declare code once, use it whenever you want.
+//                    Call the function to execute that code.
+
+
+//include parameters
+function happyBirthday(username, age){
+  console.log(`Happy birthday to you!`);
+  console.log(`Happy birthday to you!`);
+  console.log(`Happy birthday dear, ${username}`);
+  console.log(`Happy birthday to you!`);
+  console.log(`You are ${age} years old!`);
+}
+//set arguments
+console.log(happyBirthday("David", 30));
