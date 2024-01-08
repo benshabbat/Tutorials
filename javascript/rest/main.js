e40abed44a369c983de73e253371a7bf9dec3d7e
benshabbat <benshabbat27@gmail.com>
2024-01-09 00:08:32 +0200

    sum fuction and avg

--- javascript/rest/main.js
+++ javascript/rest/main.js
@@ -22,3 +22,26 @@ openFridge(food1, food2, food3, food4);
 
 const foods = getFood(food1, food2, food3, food4);
 
+// ------- EXAMPLE 2 -------
+function sum(...numbers){
+
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result;
+}
+
+function getAverage(...numbers){
+
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result / numbers.length;
+}
+
+const average = getAverage(75, 100, 85, 90, 50);
+
+console.log(average);
+
