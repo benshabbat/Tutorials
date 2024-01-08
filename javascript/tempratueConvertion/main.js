0b1fa2a99e33f95994d24c9f8d253c4230496369
benshabbat <benshabbat27@gmail.com>
2024-01-08 22:09:53 +0200

    some examples

--- javascript/tempratueConvertion/main.js
+++ javascript/tempratueConvertion/main.js
@@ -13,7 +13,6 @@ function convert() {
     temp = (temp * 9) / 5 + 32;
     result.textContent = temp.toFixed(1) + "°F";
   } else if (toCelsius.checked) {
-    // temp = Number(textBox.value);
     temp = (temp - 32) * (5 / 9);
     result.textContent = temp.toFixed(1) + "°C";
   } else {
