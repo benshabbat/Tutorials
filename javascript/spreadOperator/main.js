f9008789f965430e5324e48f70723e06559bf08d
benshabbat <benshabbat27@gmail.com>
2024-01-08 23:53:08 +0200

    ex1

---
+++ javascript/spreadOperator/main.js
@@ -0,0 +1,12 @@
+// spread operator =  ... allows an iterable such as an
+//                                  array or string to be expanded
+//                                   in to separate elements
+//                                  (unpacks the elements)
+
+// ------------ EXAMPLE 1 ------------
+let numbers = [1, 2, 3, 4, 5];
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+console.log(maximum);
+
