9d5a660a65c9869042631a2f85695f3b6e4678c6
benshabbat <benshabbat27@gmail.com>
2024-01-08 23:53:26 +0200

    ex3

--- javascript/spreadOperator/main.js
+++ javascript/spreadOperator/main.js
@@ -16,3 +16,9 @@ let letters = [...username];
 
 console.log(letters);
 
+// ------------ EXAMPLE 3 ------------
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potatoes"];
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
