5f596c086b2bf58e767c0bd54c7213b8f1865486
benshabbat <benshabbat27@gmail.com>
2024-01-08 21:45:25 +0200

    done css

--- javascript/tempratueConvertion/main.js
+++ javascript/tempratueConvertion/main.js
@@ -6,19 +6,17 @@ const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
 let temp;
 
-function convert(){
-
-    if(toFahrenheit.checked){
-        temp = Number(textBox.value);
-        temp = temp * 9 / 5 + 32;
-        result.textContent = temp.toFixed(1) + "°F";
-    }
-    else if(toCelsius.checked){
-        temp = Number(textBox.value);
-        temp = (temp - 32) * (5/9);
-        result.textContent = temp.toFixed(1) + "°C";
-    }
-    else{
-        result.textContent = "Select a unit"; 
-    }
+function convert() {
+  console.log(textBox.value , typeof textBox.value);
+  temp = Number(textBox.value);
+  if (toFahrenheit.checked) {
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    // temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Select a unit";
+  }
 }
