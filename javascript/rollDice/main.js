03e2f332d48cdea3de697e90166c602ff8505aea
benshabbat <benshabbat27@gmail.com>
2024-01-10 20:32:46 +0200

    done

--- javascript/rollDice/main.js
+++ javascript/rollDice/main.js
@@ -1,15 +1,17 @@
 // DICE ROLLER PROGRAM
 
 function rollDices() {
-const numOfDices = document.getElementById("numOfDices").value;
-const dicesRes = document.getElementById("dicesRes");
-const dices=[];
+  const numOfDices = document.getElementById("numOfDices").value;
+  const dicesRes = document.getElementById("dicesRes");
+  const dicesImg = document.getElementById("dicesImg");
+  const dices = [];
+  const images = [];
 
-    for(let i = 0; i < numOfDices; i++){
-        const diceRandom = Math.floor(Math.random() * 6) + 1;
-        dices.push(diceRandom);
-        dicesRes.textContent = dices;
-    }
+  for (let i = 0; i < numOfDices; i++) {
+    const diceRandom = Math.floor(Math.random() * 6) + 1;
+    dices.push(diceRandom);
+    images.push(`<img src="images/dice${diceRandom}.png" alt="Dice ${diceRandom}">`);
+  }
+  dicesRes.textContent = dices;
+  dicesImg.innerHTML = images.join('');
 }
-
-
