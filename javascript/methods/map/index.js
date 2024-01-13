afaed11d4f995950a12d443b545eb9bbc461059a
benshabbat <benshabbat27@gmail.com>
2024-01-13 19:52:20 +0200

    ex2

--- javascript/methods/map/index.js
+++ javascript/methods/map/index.js
@@ -16,3 +16,18 @@ function cube(element){
     return Math.pow(element, 3);
 }
 
+// ------------ EXAMPLE 2 ------------
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+console.log(studentsLower);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+
