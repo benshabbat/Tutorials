9ae60cbb25e22a3487f427c5e0e54a2dfb9498b4
benshabbat <benshabbat27@gmail.com>
2024-01-13 19:56:12 +0200

    ex4

--- javascript/methods/map/index.js
+++ javascript/methods/map/index.js
@@ -1,4 +1,4 @@
-// .map() = accepts a callback and applies that function 
+// .map() = accepts a callback and applies that function
 //                 to each element of an array, then return a new array
 
 // ------------ EXAMPLE 1 ------------
@@ -8,12 +8,12 @@ const cubed = numbers.map(cube);
 
 console.log(cubed);
 
-function square(element){
-    return Math.pow(element, 2);
+function square(element) {
+  return Math.pow(element, 2);
 }
 
-function cube(element){
-    return Math.pow(element, 3);
+function cube(element) {
+  return Math.pow(element, 3);
 }
 
 // ------------ EXAMPLE 2 ------------
@@ -23,12 +23,12 @@ const studentsLower = students.map(lowerCase);
 
 console.log(studentsLower);
 
-function upperCase(element){
-    return element.toUpperCase();
+function upperCase(element) {
+  return element.toUpperCase();
 }
 
-function lowerCase(element){
-    return element.toLowerCase();
+function lowerCase(element) {
+  return element.toLowerCase();
 }
 
 // ------------ EXAMPLE 3 ------------
@@ -37,7 +37,19 @@ const formattedDates = dates.map(formatDates);
 
 console.log(formattedDates);
 
-function formatDates(element){
-    const parts = element.split("-");
-    return `${parts[2]}/${parts[1]}/${parts[0]}`;
+function formatDates(element) {
+  const parts = element.split("-");
+  return `${parts[2]}/${parts[1]}/${parts[0]}`;
 }
+
+// ------------ EXAMPLE 4 ------------
+const numbers2 = [1, 2, 3, 4, 5];
+const squared2 = numbers.map((num) => {
+  return Math.pow(num, 2);
+});
+const cubed2 = numbers.map((num) => {
+  return Math.pow(num, 3);
+});
+
+console.log(squared2)
+console.log(cubed2)
