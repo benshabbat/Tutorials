619db18225e0dbb6b69f5fe65de44cec4d1defcc
benshabbat <benshabbat27@gmail.com>
2024-01-13 21:26:22 +0200

    ex 1

---
+++ javascript/methods/reduce/index.js
@@ -0,0 +1,13 @@
+// .reduce() = reduce the elements of an array
+//                     to a single value    
+
+// ----------- EXAMPLE 1 -----------
+const prices = [5, 30, 10, 25, 15, 20];
+const total = prices.reduce(sum);
+
+console.log(`$${total.toFixed(2)}`);
+
+function sum(accumulator, element){
+    return accumulator + element;
+}
+
