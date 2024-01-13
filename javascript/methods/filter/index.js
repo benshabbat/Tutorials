f63f29d1c888755f12ef36aff5b516b23068f9a0
benshabbat <benshabbat27@gmail.com>
2024-01-13 20:06:33 +0200

    ex2

--- javascript/methods/filter/index.js
+++ javascript/methods/filter/index.js
@@ -15,4 +15,18 @@ function isEven(element){
 function isOdd(element){
     return element % 2 !== 0;
 }
+// ----------- EXAMPLE 2 -----------
+let ages = [16, 17, 17, 18, 19, 20, 65];
+let adults = ages.filter(isAdult);
+let children = ages.filter(isChild);
+
+console.log(children);
+
+function isAdult(element){
+    return element >= 18;
+}
+
+function isChild(element){
+    return element < 18;
+}
 
