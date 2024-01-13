978ff6757f9f9bafbf40973a2a52622936c7f57a
benshabbat <benshabbat27@gmail.com>
2024-01-13 20:06:39 +0200

    ex3

--- javascript/methods/filter/index.js
+++ javascript/methods/filter/index.js
@@ -30,3 +30,17 @@ function isChild(element){
     return element < 18;
 }
 
+// ----------- EXAMPLE 3 -----------
+const words = ['apple', 'orange', 'kiwi', 'banana', 'pomegranate', 'coconut',];
+const longWords = words.filter(getLongWords);
+const shortWords = words.filter(getShortWords);
+
+console.log(shortWords);
+
+function getShortWords(element){
+    return element.length <= 6;
+}
+
+function getLongWords(element){
+    return element.length > 6;
+}
