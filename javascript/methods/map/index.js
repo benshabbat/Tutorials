dc2bdc9456f8b79c4a88ee60112ff7a970ffbba0
benshabbat <benshabbat27@gmail.com>
2024-01-13 19:52:31 +0200

    ex3

--- javascript/methods/map/index.js
+++ javascript/methods/map/index.js
@@ -31,3 +31,13 @@ function lowerCase(element){
     return element.toLowerCase();
 }
 
+// ------------ EXAMPLE 3 ------------
+const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element){
+    const parts = element.split("-");
+    return `${parts[2]}/${parts[1]}/${parts[0]}`;
+}
