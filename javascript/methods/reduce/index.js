14f46335e5f9d0d0844ccb15527c4f51ed718f08
benshabbat <benshabbat27@gmail.com>
2024-01-13 21:26:32 +0200

    ex2

--- javascript/methods/reduce/index.js
+++ javascript/methods/reduce/index.js
@@ -11,3 +11,18 @@ function sum(accumulator, element){
     return accumulator + element;
 }
 
+// ----------- EXAMPLE 2 -----------
+const scores = [75, 50, 90, 80, 65, 95];
+const maximum = scores.reduce(getMax);
+const minimum = scores.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(accumulator, element){
+    return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element){
+    return Math.min(accumulator, element);
+}
