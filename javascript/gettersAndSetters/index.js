dd22b7e9af5bda2ac6691c620eecfd284a8680d0
benshabbat <benshabbat27@gmail.com>
2024-01-15 15:22:00 +0200

    ex for user

--- javascript/gettersAndSetters/index.js
+++ javascript/gettersAndSetters/index.js
@@ -44,3 +44,58 @@ const rectangle = new Rectangle(2, 3);
 console.log(rectangle.width);
 console.log(rectangle.height);
 console.log(rectangle.area);
+// ---------- EXAMPLE 2 ----------
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  set firstName(newFirstName) {
+    if (typeof newFirstName === "string" && newFirstName.length > 0) {
+      this._firstName = newFirstName;
+    } else {
+      console.error("First name must be a non-empty string");
+    }
+  }
+
+  set lastName(newLastName) {
+    if (typeof newLastName === "string" && newLastName.length > 0) {
+      this._lastName = newLastName;
+    } else {
+      console.error("Last name must be a non-empty string");
+    }
+  }
+
+  set age(newAge) {
+    if (typeof newAge === "number" && newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.error("Age must be a non-negative number");
+    }
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  get fullName() {
+    return this._firstName + " " + this._lastName;
+  }
+
+  get age() {
+    return this._age;
+  }
+}
+
+const person = new Person("DavidChen", "Benshabbat", 30);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
