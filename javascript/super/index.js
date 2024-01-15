055e9539028b4509ef7c7ec15c43dc26bcc8b484
benshabbat <benshabbat27@gmail.com>
2024-01-15 15:21:04 +0200

    ex1

--- javascript/super/index.js
+++ javascript/super/index.js
@@ -3,55 +3,51 @@
 //              this = this object
 //              super = the parent
 
-class Animal{
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-
-    move(speed){
-        console.log(`The ${this.name} moves at a speed of ${speed}mph`);
-    }
+  move(speed) {
+    console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+  }
 }
 
-class Rabbit extends Animal{
+class Rabbit extends Animal {
+  constructor(name, age, runSpeed) {
+    super(name, age);
+    this.runSpeed = runSpeed;
+  }
 
-    constructor(name, age, runSpeed){
-        super(name, age);
-        this.runSpeed = runSpeed;
-    }
-
-    run(){
-        console.log(`This ${this.name} can run`);
-        super.move(this.runSpeed);
-    }
+  run() {
+    console.log(`This ${this.name} can run`);
+    super.move(this.runSpeed);
+  }
 }
 
-class Fish extends Animal{
-    
-    constructor(name, age, swimSpeed){
-        super(name, age);
-        this.swimSpeed = swimSpeed;
-    }
+class Fish extends Animal {
+  constructor(name, age, swimSpeed) {
+    super(name, age);
+    this.swimSpeed = swimSpeed;
+  }
 
-    swim(){
-        console.log(`This ${this.name} can swim`);
-        super.move(this.swimSpeed);
-    }
+  swim() {
+    console.log(`This ${this.name} can swim`);
+    super.move(this.swimSpeed);
+  }
 }
 
-class Hawk extends Animal{
-    
-    constructor(name, age, flySpeed){
-        super(name, age);
-        this.flySpeed = flySpeed;
-    }
+class Hawk extends Animal {
+  constructor(name, age, flySpeed) {
+    super(name, age);
+    this.flySpeed = flySpeed;
+  }
 
-    fly(){
-        console.log(`This ${this.name} can fly`);
-        super.move(this.flySpeed);
-    }
+  fly() {
+    console.log(`This ${this.name} can fly`);
+    super.move(this.flySpeed);
+  }
 }
 
 const rabbit = new Rabbit("rabbit", 1, 25);
