8236ea84bc1cc1428d57367459d3e89f11628248
benshabbat <benshabbat27@gmail.com>
2024-01-17 00:08:09 +0200

    ex2

--- javascript/sort/index.js
+++ javascript/sort/index.js
@@ -10,5 +10,22 @@ numbers.sort((a, b) => b - a); //REVERSE
 
 console.log(numbers);
 
+// ---------- EXAMPLE 2 ----------
+const people = [
+  { name: "Spongebob", age: 30, gpa: 3.0 },
+  { name: "Patrick", age: 37, gpa: 1.5 },
+  { name: "Squidward", age: 51, gpa: 2.5 },
+  { name: "Sandy", age: 27, gpa: 4.0 },
+];
 
+people.sort((a, b) => a.age - b.age); //FORWARD
+people.sort((a, b) => b.age - a.age); //REVERSE
+
+people.sort((a, b) => a.gpa - b.gpa); //FORWARD
+people.sort((a, b) => b.gpa - a.gpa); //REVERSE
+
+people.sort((a, b) => a.name.localeCompare(b.name)); //FORWARD
+people.sort((a, b) => b.name.localeCompare(a.name)); //REVERSE
+
+people.sort()
 console.log(people);
