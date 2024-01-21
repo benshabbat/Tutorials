692cbe28e4f831095127a9f593f311badabdc61a
benshabbat <benshabbat27@gmail.com>
2024-01-21 22:52:12 +0200

    some example for date

--- javascript/date/main.js
+++ javascript/date/main.js
@@ -1,3 +1,9 @@
 // Date(year, month, day, hour, minute, second, ms)
 const date = new Date();
 console.log(date);
+//day in month
+console.log(date.getDate())
+//month in year
+console.log(date.getMonth())
+
+
