62361d4dd9b4e8db4c3292af5f7c4123c547f3c5
benshabbat <benshabbat27@gmail.com>
2024-01-22 23:18:01 +0200

    ex3

--- javascript/closure/index.js
+++ javascript/closure/index.js
@@ -47,3 +47,33 @@ counter.increment();
 
 console.log(`Current count: ${counter.getCount()}`);
 
+// ---------- EXAMPLE 3 ----------
+
+function createGame(){
+
+    let score = 0;
+
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`);
+    }
+    
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+    
+    function getScore(){
+        return score;
+    }
+
+    return {increaseScore, decreaseScore, getScore};
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+
+console.log(`The final score is ${game.getScore()}pts`);
