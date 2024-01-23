3cf230ef331a991a330b100af53dbbfdf2cc2575
benshabbat <benshabbat27@gmail.com>
2024-01-23 23:57:15 +0200

    done

--- javascript/smallProjects/digitalClock/index.js
+++ javascript/smallProjects/digitalClock/index.js
@@ -1,9 +1,13 @@
 const updateClock = () => {
   const clock = new Date();
-  const hours = clock.getHours();
-  const minutes = clock.getMinutes();
-  const seconds = clock.getSeconds();
-  document.getElementById("clock").textContent =
-    hours + ":" + minutes + ":" + seconds;
+  const hours = clock.getHours().toString.padStart(2, 0);
+  const minutes = clock.getMinutes().toString.padStart(2, 0);
+  const seconds = clock.getSeconds().toString.padStart(2, 0);
+  const time = hours + ":" + minutes + ":" + seconds;
+  //   hours = hours<10? "0"+hours:hours;
+  //   minutes = minutes<10? "0"+minutes:minutes;
+  //   seconds = seconds<10? "0"+seconds:seconds;
+  document.getElementById("clock").textContent = time;
 };
 updateClock();
+setInterval(updateClock, 1000);
