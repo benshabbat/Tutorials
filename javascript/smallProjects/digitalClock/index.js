aa41cb435c1fb1f3bd17a6c353888e1d4f9dfea2
benshabbat <benshabbat27@gmail.com>
2024-01-23 23:57:45 +0200

    fix syntax

--- javascript/smallProjects/digitalClock/index.js
+++ javascript/smallProjects/digitalClock/index.js
@@ -1,8 +1,8 @@
 const updateClock = () => {
   const clock = new Date();
-  const hours = clock.getHours().toString.padStart(2, 0);
-  const minutes = clock.getMinutes().toString.padStart(2, 0);
-  const seconds = clock.getSeconds().toString.padStart(2, 0);
+  const hours = clock.getHours().toString().padStart(2, 0);
+  const minutes = clock.getMinutes().toString().padStart(2, 0);
+  const seconds = clock.getSeconds().toString().padStart(2, 0);
   const time = hours + ":" + minutes + ":" + seconds;
   //   hours = hours<10? "0"+hours:hours;
   //   minutes = minutes<10? "0"+minutes:minutes;
