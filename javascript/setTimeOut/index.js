c80d368de61165a8aafbed4c232b7238fcbe2d3e
benshabbat <benshabbat27@gmail.com>
2024-01-25 00:41:40 +0200

    ex1

---
+++ javascript/setTimeOut/index.js
@@ -0,0 +1,12 @@
+// setTimeout() = function in JavaScript that allows you to schedule
+//                            the execution of a function after an amount of time 
+//                            Times are approximate
+//                            setTimeout(callback, delay);
+
+// ---------- EXAMPLE 1 ----------
+function hello() {
+    window.alert("Hello");
+}
+ 
+setTimeout(hello, 3000);
+
