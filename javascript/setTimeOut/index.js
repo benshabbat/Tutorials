b64616f10a8562a4df2912aad34e83078f3b3d39
benshabbat <benshabbat27@gmail.com>
2024-01-25 00:42:52 +0200

    ex3

--- javascript/setTimeOut/index.js
+++ javascript/setTimeOut/index.js
@@ -13,6 +13,24 @@ setTimeout(hello, 3000);
 // ---------- EXAMPLE 2 ----------
 // clearTimeout() = can cancel a timeout before it triggers
 
-const timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+// const timeoutId = setTimeout(() => window.alert("Hello"), 3000);
 
-clearTimeout(timeoutId);
+// clearTimeout(timeoutId);
+
+// ---------- EXAMPLE 3 ----------
+
+let timeoutId;
+
+function showAlert() {
+    window.alert("Hello");
+}
+
+function startTimer() {
+    timeoutId = setTimeout(showAlert, 3000);
+    console.log("STARTED");
+}
+
+function clearTime() {
+    clearTimeout(timeoutId);
+    console.log("CLEARED");
+}
