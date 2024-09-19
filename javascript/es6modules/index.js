80cd529011dd6adc85811308976db5d0a54b0bb4
benshabbat <benshabbat27@gmail.com>
2024-09-19 14:38:57 +0300

    ex for es6

---
+++ javascript/es6modules/index.js
@@ -0,0 +1,5 @@
+import {PI,getArea,getCircumference} from "./mathUtils.js";
+
+console.log(getArea(4).toFixed(2));
+console.log(getCircumference(4).toFixed(2));
+console.log(PI);
---
+++ javascript/es6modules/mathUtils.js
@@ -0,0 +1,9 @@
+export const PI = 3.14159
+
+
+export const getCircumference = radius =>{
+    return 2* PI * radius
+}
+export const getArea = radius =>{
+    return PI * radius*radius
+}
