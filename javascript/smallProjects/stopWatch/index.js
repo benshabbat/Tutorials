33189e4564a8c461575a4b328833ad7f870bd524
benshabbat <benshabbat27@gmail.com>
2024-09-19 11:51:25 +0300

    i dont need 0 for start time when i do reset bc every time press on start i get new val for starttime

--- javascript/smallProjects/stopWatch/index.js
+++ javascript/smallProjects/stopWatch/index.js
@@ -26,7 +26,7 @@ function stop(){
 
 function reset(){
     clearInterval(timer);
-    startTime = 0;
+    // startTime = 0;
     elapsedTime = 0;
     isRunning = false;    
     display.textContent = "00:00:00:00";
