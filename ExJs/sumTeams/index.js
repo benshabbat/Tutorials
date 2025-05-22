977540d670c8ea56afa5304eddcc29ee2ed2f169
benshabbat <benshabbat27@gmail.com>
2025-05-22 10:54:02 +0300

    refactor sumPositiveNumbers function to sumTeams for 2D array input

--- ExJs/sumTeams/index.js
+++ ExJs/sumTeams/index.js
@@ -1,12 +1,13 @@
-// כתוב פונקציה שמקבלת מערך מספרים ומחזירה את הסכום של כל המספרים החיוביים במערך
-function sumPositiveNumbers(arr) {
-  return arr.reduce((acc, num) => {
-    if (num > 0) {
-      return acc + num;
-    }
-    return acc; // החזר את הערך המצטבר אם המספר אינו חיובי
-  }, 0); // אתחל את הערך המצטבר ל-0
+const arr = [
+  [4, 2, 2, 4],
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+];
+
+function sumTeams(arr) {
+  return arr.map((team) => {
+    return team.reduce((acc, num) => acc + num, 0);
+  });
 }
 
-console.log(sumPositiveNumbers([1, -2, 3, -4, 5])); // 9
-// לדוגמה: sumPositiveNumbers([1, -2, 3, -4, 5]) צריך להחזיר 9
+console.log(sumTeams(arr));
