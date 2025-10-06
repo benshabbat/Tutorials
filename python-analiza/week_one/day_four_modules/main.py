653293a89fd19ceaeff0497066d1a77f643379f2
benshabbat <benshabbat27@gmail.com>
2025-10-06 12:21:12 +0300

    refactor: implement math_utils module with add and multiply functions

---
+++ python-analiza/week_one/day_four_modules/main.py
@@ -0,0 +1,7 @@
+import math_utils
+
+result_add = math_utils.add(5, 3)
+result_multiply = math_utils.multiply(5, 3)
+
+print(f"5 + 3 = {result_add}")
+print(f"5 * 3 = {result_multiply}")
---
+++ python-analiza/week_one/day_four_modules/math_utils.py
@@ -0,0 +1,11 @@
+# Create a Simple Module
+#  Write a module (math_utils.py) with two functions: add(a, b)
+#  and multiply(a, b). Import it and use it in a separate script.
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+#
