66cc31a240914d29655a2be6bf6d5e64e051913e
benshabbat <benshabbat27@gmail.com>
2025-10-06 12:26:07 +0300

    refactor: comment out math_utils functions and examples for clarity

--- python-analiza/week_one/day_four_modules/main.py
+++ python-analiza/week_one/day_four_modules/main.py
@@ -1,7 +1,25 @@
-import math_utils
+# import math_utils
 
-result_add = math_utils.add(5, 3)
-result_multiply = math_utils.multiply(5, 3)
+# result_add = math_utils.add(5, 3)
+# result_multiply = math_utils.multiply(5, 3)
 
-print(f"5 + 3 = {result_add}")
-print(f"5 * 3 = {result_multiply}")
+# print(f"5 + 3 = {result_add}")
+# print(f"5 * 3 = {result_multiply}")
+
+# Alias Import
+#  Import the math module using an alias
+#  (import math as m) and compute the square root of 16.
+
+# import math as m
+
+# print(m.sqrt(16))
+
+# Selective Import
+#  Import only the pi constant from the math module
+#  and print the area of a circle with radius 5.
+
+# from math import pi
+
+# radius = 5
+# area = pi * (radius**2)
+# print(f"Area of the circle with radius {radius}: {area}")
--- python-analiza/week_one/day_four_modules/math_utils.py
+++ python-analiza/week_one/day_four_modules/math_utils.py
@@ -2,10 +2,20 @@
 #  Write a module (math_utils.py) with two functions: add(a, b)
 #  and multiply(a, b). Import it and use it in a separate script.
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-def multiply(a, b):
-    return a * b
+# def multiply(a, b):
+#     return a * b
 
-#
+#Using Built-in Modules
+#  Write a script that uses the random module to generate 5
+#  random integers between 1 and 100.
+
+# import random
+
+# random_integers = [random.randint(1, 100) for _ in range(5)]
+# print("Random Integers:", random_integers)
+
+# for i in range(5):
+#     print(f"Random Integer {i+1}: {random.randint(1, 100)}")
