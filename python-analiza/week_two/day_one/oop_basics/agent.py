e1e20d3fcd2b5aeae6f7377c385a9a05f145c124
benshabbat <benshabbat27@gmail.com>
2025-10-13 15:02:30 +0300

    fix: update clearance level handling in Agent class to use private attribute

--- python-analiza/week_two/day_one/oop_basics/agent.py
+++ python-analiza/week_two/day_one/oop_basics/agent.py
@@ -1,11 +1,54 @@
 class Agent:
     def __init__(self, code_name:str , clearance_level : int):
         self.code_name = code_name
-        self.clearance_level = clearance_level
+        self.__clearance_level = clearance_level
         
     def report(self):
-        print(f"Agent {self.code_name} reporting. Clearance Level: {self.clearance_level}")
-        
-        
-     
-        
+        print(f"Agent {self.code_name} reporting. Clearance Level: {self.__clearance_level}")
+
+       
+    def set_clearance_level(self, new_level: int):
+        if new_level < 1 or new_level > 5:
+            print("Clearance level must be between 1 and 5.")
+        else:
+            self.__clearance_level = new_level
+            print(f"Clearance level updated to {self.__clearance_level}")
+            
+            
+    def get_clearance_level(self):
+        print(f"Current clearance level: {self.__clearance_level}")
+        return self.__clearance_level
+    
+    
+ag = Agent("Eagle", 3)
+ag.report()
+ag.set_clearance_level(4)
+print(ag.get_clearance_level())
+
+
+
+#     @property
+#     def clearance_level(self):
+#         """Getter for clearance level"""
+#         print(f"Current clearance level: {self.__clearance_level}")
+#         return self.__clearance_level
+    
+#     @clearance_level.setter
+#     def clearance_level(self, new_level: int):
+#         """Setter for clearance level"""
+#         if new_level < 1 or new_level > 5:
+#             print("Clearance level must be between 1 and 5.")
+#         else:
+#             self.__clearance_level = new_level
+#             print(f"Clearance level updated to {self.__clearance_level}")
+    
+    
+# ag = Agent("Eagle", 3)
+# ag.report()
+
+# # עכשיו נוכל לגשת ל-clearance_level כמו משתנה רגיל
+# print(f"Getting clearance level: {ag.clearance_level}")  # קורא ל-getter
+# ag.clearance_level = 4  # קורא ל-setter
+# print(f"After update: {ag.clearance_level}")  # קורא ל-getter שוב
+    
+    
